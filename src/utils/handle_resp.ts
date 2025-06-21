e84fb62031092bbba8358c5273c73ffdb8948dd0
lyy2005a3 <long2005a3@outlook.com>
2025-06-21 15:00:40 +0000

    更新api

--- src/utils/handle_resp.ts
+++ src/utils/handle_resp.ts
@@ -15,7 +15,7 @@ export const handleResp = <T>(
   } else {
     notify_error && notify.error(resp.message)
     if (auth && resp.code === 401) {
-      if (location.pathname === "/LONGYun2025") {
+      if (location.pathname === "/@manage") {
         bus.emit("to", "/")
       } else {
         bus.emit(
