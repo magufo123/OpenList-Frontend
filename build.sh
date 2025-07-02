c9074709c175bd2993f8ca0317025a3de4b6b31f
lyy2005a3 <long2005a3@outlook.com>
2025-07-02 06:42:41 +0000

    更新package.json

--- build.sh
+++ build.sh
@@ -159,7 +159,7 @@ fetch_i18n_from_release() {
     if echo -n "$release_response" | grep -q "Not Found"; then
         log_warning "Failed to fetch release info. Skipping i18n fetch."
     else
-        iextract_i18n_tarball "$release_response"
+        extract_i18n_tarball "$release_response"
     fi
 }
 
